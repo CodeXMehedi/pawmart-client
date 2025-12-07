cbaea22356f81a1b5dfc59e11a4fff76ca283107
CodeXMehedi <khkmehedi.desk@gmail.com>
2025-12-08 02:06:38 +0600

    added pet and supplies page and all service card

--- src/Pages/PetsSupplies/PetsSupplies.jsx
+++ src/Pages/PetsSupplies/PetsSupplies.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from '../../components/ServiceCard';
 
 const PetsSupplies = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/services')
+      .then(res => res.json())
+      .then(data => setServices(data))
+    .catch(err=>console.log(err))
+  }, [])
+  console.log(services)
   return (
-    <div>
-      pets & supplies
-    </div>
+    <>
+      <h1 className="text-4xl font-bold text-center text-[#A55E2A] my-8">
+        Explore Pets & Supplies <br /> For Your Furry Friends
+        </h1>
+    <div className='grid grid-cols-3 w-10/12 m-auto gap-4'>
+      {
+        services.map(service=><ServiceCard key={service._id} service={service}> </ServiceCard>)
+      }
+      </div>
+      </>
   );
 };
 
