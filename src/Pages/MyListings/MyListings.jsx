1801201a00753acedcc6ba1bfd786fa9a21eb53a
CodeXMehedi <khkmehedi.desk@gmail.com>
2025-12-08 07:56:40 +0600

    added MyListing page

--- src/Pages/MyListings/MyListings.jsx
+++ src/Pages/MyListings/MyListings.jsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const MyListings = () => {
+  const [myServices, setMyServices] = useState([]);
+  const { user } = useContext(AuthContext);
+  
+  useEffect(() => {
+    fetch(`http://localhost:3000/myServices?email=${user?.email}`)
+      .then(res => res.json())
+      .then(data => setMyServices(data))
+      .catch(err => console.log(err))
+  }, [user?.email])
+  
   return (
-    <div>
-      my listing
+    <div className="overflow-x-auto mt-8  w-10/12 m-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th className='pl-18'>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {myServices.map((service) => (
+            <tr key={service?.id}>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={service?.image}
+                        alt="User Avatar"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">{service?.name}</div>
+                  </div>
+                </div>
+              </td>
+
+              <td>{service?.description}</td>
+
+              <td>${service?.price}</td>
+
+              <th >
+                <button className="btn btn-outline btn-xs mr-2">Edit</button>
+                <button className="btn btn-outline btn-xs">Delete</button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
